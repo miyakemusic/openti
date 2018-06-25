aa7554764f35ebf52ff6c4de99df55b2b1c37f5b
miyakemusic <miyakemusic@yahoo.co.jp>
2018-06-25 23:47:39 +0900

    update

---
+++ openti/src/main/java/openti/OpenTiApplication.java
@@ -0,0 +1,15 @@
+package openti;
+
+
+import jp.silverbullet.BuilderFx;
+
+public class OpenTiApplication extends BuilderFx {
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+	@Override
+	protected String getBackupFilename() {
+		return "osa_back.zip";
+	}
+}
