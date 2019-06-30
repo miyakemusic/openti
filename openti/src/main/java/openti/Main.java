b9edca39d0516c2f79cb950a5c2d527a3bdd7221
miyak <miyak@miyake-surface>
2019-06-30 18:33:57 +0900

    up

--- openti/src/main/java/openti/Main.java
+++ openti/src/main/java/openti/Main.java
@@ -45,7 +45,7 @@ public class Main extends SilverBulletServer {
 	@Override
 	protected List<UserSequencer> getUserSequencers(BuilderModelImpl model) {
 		return Arrays.asList(new TestSequencer(), new OscilloTestSequencer(), 
-				new SystemManager(), new CursorHandler(), new FileManager());
+				new SystemManager(), new FileManager());
 	}
 //	@Override
 	protected UiBuilder getUi2() {
